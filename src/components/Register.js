95b243fd85fb7c8e315acde4525a75a5bf2498c0
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-02-14 11:35:51 +0300

    fix: поправить импорт

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,12 +1,13 @@
 import React from "react";
-import Link from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import AuthorizeForm from "./AuthorizeForm";
 
 function Register(props) {
 
   return (
     <section className="authorize">
-      <AuthorizeForm title={props.title} onSubmit={props.onRegister} textOfButton={props.textOfButton}/>
+       {/* onSubmit={props.?} */}
+      <AuthorizeForm title={props.title} textOfButton={props.textOfButton}/>
       <div className="authorize__container">
         <p className="authorize__text">Уже зарегистрированы?</p>
         <Link to="/sign-in" className="authorize__link link">Войти</Link>
