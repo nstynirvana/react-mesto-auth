91aa84ab97e9c1bee5d93314e983489e36065bb8
Anastasia Savinova <nsty.nirvana@gmail.com>
2023-02-19 17:05:41 +0300

    fix:тестовый пуш

--- src/components/App.js
+++ src/components/App.js
@@ -270,7 +270,7 @@ function App() {
 
         <Routes>
           <Route
-            path="/*"
+            path="/"
             element={
               <ProtectedRoute
                 isLoggedIn={isLoggedIn}
